8b90f3da5daa8960483ffb208a8273d50ae6e3b6
Alfredo Sierra <alfredosierra1999@gmail.com>
2022-03-01 13:07:23 -0600

    feat views maked

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Ingreso from './pages/Ingreso';
+import Main from './pages/Main';
+import Vista from './pages/Vista';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route exact path="/vista" element={<Vista />} />
+        <Route exact path="/ingreso" element={<Ingreso />} />
+      </Routes>
+    </Router>
+
   );
 }
 
